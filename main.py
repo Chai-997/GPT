5385686f17494a013d776519fe4f3ea7ba45fbf9
Charlie <charlie.allen97@hotmail.com>
2024-05-13 17:16:06 +0100

    gen (no attention)

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import torch # PyTorch: https://pytorch.org
+import torch.nn as nn
+from torch.nn import functional as F
+import BigramLanguageModel
 
 # read it in to inspect it
 with open('input.txt', 'r', encoding='utf-8') as f:
@@ -12,9 +15,9 @@ with open('input.txt', 'r', encoding='utf-8') as f:
 # here are all the unique characters that occur in this text
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
-print("Available characters:")
-print(''.join(chars))
-print(vocab_size, '\n')
+# print("Available characters:")
+# print(''.join(chars))
+# print(vocab_size, '\n')
 
 # create a mapping from characters to integers (tokeniser)
 stoi = { ch:i for i,ch in enumerate(chars) }
@@ -22,10 +25,104 @@ itos = { i:ch for i,ch in enumerate(chars) }
 encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
 decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
 
-print(encode("hii there"))
-print(decode(encode("hii there")), '\n')
+# print(encode("Hi there!"))
+# print(decode(encode("Hi there!")), '\n')
 
 # let's now encode the entire text dataset and store it into a torch.Tensor
 data = torch.tensor(encode(text), dtype=torch.long)
-print(data.shape, data.dtype)
-print(data[:1000]) # the 1000 characters we looked at earier will to the GPT look like this
+# print(data.shape, data.dtype)
+# print(data[:1000]) # the 1000 characters we looked at earier will to the GPT look like this
+
+# Let's now split up the data into train and validation sets
+n = int(0.9*len(data)) # first 90% will be train, rest val
+train_data = data[:n]
+val_data = data[n:]
+
+block_size = 8
+train_data[:block_size+1]
+
+x = train_data[:block_size]
+y = train_data[1:block_size+1]
+for t in range(block_size):
+    context = x[:t+1]
+    target = y[t]
+    # print(f"when input is {context} the target: {target}")
+
+torch.manual_seed(1337)
+batch_size = 4 # how many independent sequences will we process in parallel?
+block_size = 8 # what is the maximum context length for predictions?
+
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x, y
+
+xb, yb = get_batch('train')
+# print('inputs:')
+# print(xb.shape)
+# print(xb)
+# print('targets:')
+# print(yb.shape)
+# print(yb)
+
+for b in range(batch_size): # batch dimension
+    for t in range(block_size): # time dimension
+        context = xb[b, :t+1]
+        target = yb[b,t]
+        # print(f"when input is {context.tolist()} the target: {target}")
+
+# print('----')
+print(xb) # our input to the 
+
+m = BigramLanguageModel.BigramLanguageModel(vocab_size)
+logits, loss = m(xb, yb)
+# print(logits.shape)
+# print(loss)
+
+# create a PyTorch optimizer
+optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
+
+batch_size = 32
+for steps in range(10000): # increase number of steps for good results...
+
+    # sample a batch of data
+    xb, yb = get_batch('train')
+
+    # evaluate the loss
+    logits, loss = m(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+# print("\nloss:", loss.item())
+# print('\n', decode(m.generate(idx = torch.zeros((1, 1), dtype=torch.long), max_new_tokens=500)[0].tolist()))
+
+# version 4: self-attention!
+torch.manual_seed(1337)
+B,T,C = 4,8,32 # batch, time, channels
+x = torch.randn(B,T,C)
+
+# let's see a single Head perform self-attention
+head_size = 16
+key = nn.Linear(C, head_size, bias=False)
+query = nn.Linear(C, head_size, bias=False)
+value = nn.Linear(C, head_size, bias=False)
+k = key(x)   # (B, T, 16)
+q = query(x) # (B, T, 16)
+wei =  q @ k.transpose(-2, -1) # (B, T, 16) @ (B, 16, T) ---> (B, T, T)
+
+tril = torch.tril(torch.ones(T, T))
+#wei = torch.zeros((T,T))
+wei = wei.masked_fill(tril == 0, float('-inf'))
+wei = F.softmax(wei, dim=-1)
+
+v = value(x)
+out = wei @ v
+#out = wei @ x
+
+out.shape
+
+print(wei)
